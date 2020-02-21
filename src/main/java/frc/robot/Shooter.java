177e10d52b1fe4c96e97213685a7c7f0ef440335
Plasma <rusk.alex4@gmail.com>
2020-02-21 16:51:03 -0700

    Changed frontRollerMotor from Talon to Victor. Removed backMotorRoller.

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -11,37 +11,21 @@ public class Shooter {
     TalonFX leftFlyWheelMotor;
     TalonFX rightFlyWheelMotor;
     TalonSRX hoodMotor;
-<<<<<<< HEAD
     VictorSPX frontRollerMotor;
-    TalonSRX backRollerMotor;
-=======
-    TalonSRX frontRollerMotor;
     //TalonSRX backRollerMotor;
->>>>>>> ddd4999917806225bc6b9ab69b997cfa141946dd
     
     Shooter(final int LEFT_FLY_WHEEL_MOTOR_ID, final int RIGHT_FLY_WHEEL_MOTOR_ID, final int HOOD_MOTOR_ID,
             final int FRONT_ROLLER_MOTOR_ID) {
         leftFlyWheelMotor = new TalonFX(LEFT_FLY_WHEEL_MOTOR_ID);
         rightFlyWheelMotor = new TalonFX(RIGHT_FLY_WHEEL_MOTOR_ID);
         hoodMotor = new TalonSRX(HOOD_MOTOR_ID);
-<<<<<<< HEAD
         frontRollerMotor = new VictorSPX(FRONT_ROLLER_MOTOR_ID);
-        backRollerMotor = new TalonSRX(BACK_ROLLER_MOTOR_ID);
-=======
-        frontRollerMotor = new TalonSRX(FRONT_ROLLER_MOTOR_ID);
-        // backRollerMotor = new TalonSRX(BACK_ROLLER_MOTOR_ID);
->>>>>>> ddd4999917806225bc6b9ab69b997cfa141946dd
-
+        //backRollerMotor = new TalonSRX(BACK_ROLLER_MOTOR_ID);
+        frontRollerMotor = new VictorSPX(FRONT_ROLLER_MOTOR_ID);
         limitCurrent(leftFlyWheelMotor);
         limitCurrent(rightFlyWheelMotor);
         limitCurrent(hoodMotor);
-<<<<<<< HEAD
-        limitCurrent(backRollerMotor);
-=======
-        limitCurrent(frontRollerMotor);
         // limitCurrent(backRollerMotor);
->>>>>>> ddd4999917806225bc6b9ab69b997cfa141946dd
-
         leftFlyWheelMotor.setInverted(false);
         rightFlyWheelMotor.setInverted(true);
         hoodMotor.setInverted(false);
